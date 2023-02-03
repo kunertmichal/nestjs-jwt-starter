773207601c3b5959c44dbdad33af7186396cf7f1
Michal Kunert <30593515+kunertmichal@users.noreply.github.com>
2023-02-03 13:55:52 +0100

    remove redundant files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { UsersModule } from './users/users.module';
 import { IamModule } from './iam/iam.module';
@@ -24,7 +22,5 @@ import { IamModule } from './iam/iam.module';
     }),
     IamModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
